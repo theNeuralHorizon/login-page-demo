20b5b5caa0090f05b3b8eb17c407984bce7148ea
theNeuralHorizon <kshitij.betwal@gmail.com>
2025-07-02 04:41:44 +0530

    Update Login.jsx to use Cognito Hosted UI with fixed client ID and redirect

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-const clientId = "3ei4p15ensv662vs08ioklnnkq"; // From your new app client
-const domain = "https://eu-north-1tyieklhfi.auth.eu-north-1.amazoncognito.com";
-const redirectUri = "http://localhost:3000";
-
 const Login = () => {
+  const CLIENT_ID = "3ei4p15ensv662vs08ioklnnkq";
+  const REDIRECT_URI = "http://localhost:3000";
+  const DOMAIN = "https://eu-north-1tyieklhfi.auth.eu-north-1.amazoncognito.com";
+  const RESPONSE_TYPE = "code";
+  const SCOPE = "email openid";
+
+  const loginUrl = `${DOMAIN}/login?client_id=${CLIENT_ID}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}&redirect_uri=${REDIRECT_URI}`;
+
   const handleLogin = () => {
-    const loginUrl = `${domain}/login?client_id=${clientId}&response_type=code&scope=email+openid&redirect_uri=${redirectUri}`;
     window.location.href = loginUrl;
   };
 
