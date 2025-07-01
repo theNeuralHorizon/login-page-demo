9d4efa80b6f04df27efd5a7e782e98303c8cc625
theNeuralHorizon <kshitij.betwal@gmail.com>
2025-07-02 04:42:21 +0530

    Fix App.js to import Login correctly and wrap in Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
