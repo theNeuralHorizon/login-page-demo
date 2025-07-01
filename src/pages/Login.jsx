b3f16b178a15adef9e6c24009e9d112d01b04e5f
theNeuralHorizon <kshitij.betwal@gmail.com>
2025-07-02 03:50:43 +0530

    Update login flow with Cognito app client login-spa-vercel

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-const clientId = "3ei4p15ensv662vs08ioklnnkq";
+const clientId = "3ei4p15ensv662vs08ioklnnkq"; // From your new app client
 const domain = "https://eu-north-1tyieklhfi.auth.eu-north-1.amazoncognito.com";
+const redirectUri = "http://localhost:3000";
 
 const Login = () => {
   const handleLogin = () => {
-    const redirectUri = "http://localhost:3000";
     const loginUrl = `${domain}/login?client_id=${clientId}&response_type=code&scope=email+openid&redirect_uri=${redirectUri}`;
     window.location.href = loginUrl;
   };
 
   return (
-    <div className="login-container">
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
       <h2>Login Page</h2>
       <button onClick={handleLogin}>Sign in with Cognito</button>
     </div>
