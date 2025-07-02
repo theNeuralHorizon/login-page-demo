37374c2aee64960d33065dca60c0cfd3b2b2cd35
theNeuralHorizon <kshitij.betwal@gmail.com>
2025-07-02 18:54:36 +0530

    pewpew

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,9 @@
 // src/App.test.js
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
 import App from "./App";
 
 test("renders login page on default route", () => {
-  render(
-    <MemoryRouter initialEntries={["/"]}>
-      <App />
-    </MemoryRouter>
-  );
+  render(<App />);
   expect(screen.getByText(/Login/i)).toBeInTheDocument();
 });
